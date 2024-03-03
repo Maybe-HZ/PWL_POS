3659244b832b4835c5482b63325f97584d8e5cbb
Maybe-HZ <ahmadsaifulmutaqiazis1@gmail.com>
2024-03-04 00:16:47 +0700

    JS3-Praktikum 4

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LevelController extends Controller
+{
+    public function index()
+    {
+        // pengerjaan Jobsheet 3 Praktikum 4 bagian 3
+        // DB::insert("INSERT INTO m_level(level_kode,level_nama,created_at) VALUES(?,?,?)", ['CUS', 'Pelanggan', now()]);
+        // return 'Insert Data Berhasil';
+
+        // pengerjaan Jobsheet 3 Praktikum 4 bagian 5
+        // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // return 'Update Data Berhasil. Jumlah data yang diupdate: ' . $row. ' baris';
+
+        // pengerjaan Jobsheet 3 Praktikum 4 bagian 7
+        // $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        // return 'Delete Data Berhasil. Jumlah data yang dihapus: ' . $row. ' baris';
+
+        // pengerjaan Jobsheet 3 Praktikum 4 bagian 8
+        $data = DB::select('select * from m_level');
+        return view('level.level', ['data' => $data]);
+    }
+}
